e56991048b7bd7a3d2919ca8dcc99a12bb10fba4
bbaldino <bbaldino@gmail.com>
2022-12-22 22:24:18 -0800

    add neogen plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -61,6 +61,16 @@ require('packer').startup(function(use)
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
 
+  use {
+    "danymat/neogen",
+    config = function()
+      require('neogen').setup({ snippet_engine = "luasnip" })
+    end,
+    requires = "nvim-treesitter/nvim-treesitter",
+    -- Uncomment next line if you want to follow only stable versions
+    -- tag = "*"
+  }
+
   -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
   local has_plugins, plugins = pcall(require, 'custom.plugins')
   if has_plugins then
