f7fdadde02fe44e706928f8a1e4c01583e579fc7
bbaldino <bbaldino@localhost.localdomain>
2025-07-09 20:51:40 -0700

    add kitty and fish configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fish_add_path /home/bbaldino/.local/share/bob/nvim-bin
+set -g fish_key_bindings fish_vi_key_bindings
