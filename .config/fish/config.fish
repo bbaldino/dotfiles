fb7b516d98f29d163de0a7a69964e70719529dc1
bbaldino <bbaldino@gmail.com>
2026-02-09 11:21:17 -0800

    updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,14 @@
 if status is-interactive
     fish_add_path /home/bbaldino/.cargo/bin
     fish_add_path /home/bbaldino/.local/share/bob/nvim-bin/
+    fish_add_path /home/bbaldino/.local/bin
     alias config='/usr/bin/git --git-dir=/home/bbaldino/.cfg/ --work-tree=/home/bbaldino'
-    export EDITOR=nvim
+    set -g fish_key_bindings fish_vi_key_bindings
+    set -gx EDITOR nvim
+    set -gx VISUAL nvim
 end
-export PATH="$HOME/.local/bin:$PATH"
+
+# machine-specific overrides
+if test -f ~/.config/fish/config.local.fish
+    source ~/.config/fish/config.local.fish
+end
